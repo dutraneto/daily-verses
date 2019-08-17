30dc8e68567a4e1e4a32c26bda3711cfbcb00877
DutraNeto <dudutraneto@gmail.com>
2019-08-17 02:18:44 -0400

    Updates

---
+++ precache-manifest.6a2101989733172646d89c4e9b48821e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d43f5d19b1749aa63da2e62736a464e",
+    "url": "/bible-verses/index.html"
+  },
+  {
+    "revision": "3ea08275cad684a2690c",
+    "url": "/bible-verses/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "34283ade864bd82249e7",
+    "url": "/bible-verses/static/css/main.707f661c.chunk.css"
+  },
+  {
+    "revision": "3ea08275cad684a2690c",
+    "url": "/bible-verses/static/js/2.cc884b6b.chunk.js"
+  },
+  {
+    "revision": "34283ade864bd82249e7",
+    "url": "/bible-verses/static/js/main.33211d2e.chunk.js"
+  },
+  {
+    "revision": "9136e03f32e37f783c60",
+    "url": "/bible-verses/static/js/runtime~main.55366f3d.js"
+  }
+]);
